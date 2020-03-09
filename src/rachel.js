8d52f0451fc43acc0d68fc2ec2fe83852194037e
RachelIsrael <racheleisrael4@gmail.com>
2020-03-09 14:29:16 -0400

    test

---
+++ src/rachel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class rachel extends Component {
+    render() {
+        return (
+            <div>
+               Rachel 
+            </div>
+        );
+    }
+}
+
+export default rachel;
