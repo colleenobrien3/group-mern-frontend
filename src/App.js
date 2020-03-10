bf327d88877e7423e3701ec9e7ed17751122120a
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-10 10:46:33 -0400

    nav bar styling transition

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Button from "./storybookComponents/Button/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Home</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        <div class="hamburger"></div>
-      </header>
-    </div>
-  );
+// function openNav() {
+//   document.querySelector({ nav }).style.width = "20%";
+// }
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  showNav = e => {
+    e.preventDefault();
+    alert("hi");
+  };
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Button label="Home" />
+          <img src={logo} className="App-logo" alt="logo" />
+          <button label="---" onClick={this.showNav}>
+            hi
+          </button>
+        </header>
+        <nav ref={c => (this._nav = c)}>
+          {/* <p>Home</p>
+          <p>Profile</p> */}
+        </nav>
+      </div>
+    );
+  }
 }
 
 export default App;
