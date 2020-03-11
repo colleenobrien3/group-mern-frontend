c1286dc05a2185299ca8b131f0fc23a5978d0395
Caio Ingber <57416150+caioingber@users.noreply.github.com>
2020-03-11 19:11:24 -0400

    Revert "changed card to function component"

--- src/storybookComponents/RecipeCard/RecipeCard.js
+++ src/storybookComponents/RecipeCard/RecipeCard.js
@@ -1,49 +1,65 @@
-import React from "react";
+import React, { useState, Component } from "react";
 import "./RecipeCard.css";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { render } from "@testing-library/react";
 
-const RecipeCard = props => {
-  let recipe = props.data;
-  console.log(recipe);
+class RecipeCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      deleted: false
+    };
+  }
 
-  let steps = recipe.steps.map((item, index) => {
-    return <li key={index}>{item}</li>;
-  });
-  let ingredients = recipe.ingredients.map((item, index) => {
+  render() {
+    let recipe = this.props.data;
+    console.log(recipe);
+
+    let steps = recipe.steps.map((item, index) => {
+      return <li key={index}>{item}</li>;
+    });
+    let ingredients = recipe.ingredients.map((item, index) => {
+      return (
+        <li
+          key={index}
+        >{`${item.measurementValue} ${item.measurementType} of ${item.name}`}</li>
+      );
+    });
+
+    let picture = {
+      backgroundImage: `url(${recipe.image})`
+    };
     return (
-      <li
-        key={index}
-      >{`${item.measurementValue} ${item.measurementType} of ${item.name}`}</li>
-    );
-  });
-
-  let picture = {
-    backgroundImage: `url(${recipe.image})`
-  };
-  return (
-    <div className="recipeCard">
-      <div className="delete" onClick={props.delete} id={props.data._id}></div>
-      <h2 className="recipeName">{recipe.name}</h2>
-      <div className="card-top">
-        <div className="card-content">
-          <p className="bold">Author: {recipe.author}</p>
-          <p className="bold">Cuisine: {recipe.cuisine}</p>
-          <p className="bold">Course: {recipe.course}</p>
-          <p className="bold">Cooktime: {recipe.cooktime}</p>
+      <div className="recipeCard">
+        <div
+          className="delete"
+          onClick={this.props.delete}
+          id={this.props.data._id}
+        ></div>
+        <h2 className="recipeName">{recipe.name}</h2>
+        <div className="card-top">
+          <div className="card-content">
+            <p className="bold">Author: {recipe.author}</p>
+            <p className="bold">Cuisine: {recipe.cuisine}</p>
+            <p className="bold">Course: {recipe.course}</p>
+            <p className="bold">Cooktime: {recipe.cooktime}</p>
+          </div>
+          <div className="card-picture" style={picture}></div>
         </div>
-        <div className="card-picture" style={picture}></div>
+        <div className="spanContainer">
+          <span className="cardSpan ingredient-list">
+            <p>Ingredients:</p>
+            <ul>{ingredients}</ul>
+          </span>
+          <span className="cardSpan step-list">
+            <p>Steps:</p>
+            <ol>{steps}</ol>
+          </span>
+        </div>
       </div>
-      <div className="spanContainer">
-        <span className="cardSpan ingredient-list">
-          <p>Ingredients:</p>
-          <ul>{ingredients}</ul>
-        </span>
-        <span className="cardSpan step-list">
-          <p>Steps:</p>
-          <ol>{steps}</ol>
-        </span>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default RecipeCard;
