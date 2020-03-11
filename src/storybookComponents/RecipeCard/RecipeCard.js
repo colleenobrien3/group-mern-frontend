884cb09e0aba57a3ab1f7122a68d4453bc15ee46
Colleen_O'Brien <colleen3117@gmail.com>
2020-03-11 11:07:24 -0400

    passing data through to cards

--- src/storybookComponents/RecipeCard/RecipeCard.js
+++ src/storybookComponents/RecipeCard/RecipeCard.js
@@ -2,28 +2,33 @@ import React from "react";
 import "./RecipeCard.css";
 
 const RecipeCard = props => {
+  console.log(props);
   // let classList = "";
   // if (props.large) {
   //   classList += ` button-large`;
   // }
-  let steps = props.steps.map(item => {
-    return <li>{item}</li>;
-  });
-  let ingredients = props.ingredients.map(item => {
-    return <li>{item}</li>;
-  });
+  //   let steps = props.steps.map(item => {
+  //     return <li>{item}</li>;
+  //   });
+  //   let ingredients = props.ingredients.map(item => {
+  //     return <li>{item}</li>;
+  //   });
 
   return (
     <div class="recipeCard">
       <h2 className="recipeName">{props.title}</h2>
+      <p>Author: {props.author}</p>
+      <h3>Cuisine: {props.cuisine}</h3>
+      <h3>Course: {props.course}</h3>
+      <h3>Cooktime: {props.cooktime}</h3>
       <div className="spanContainer">
         <span className="cardSpan ingredient-list">
           <p>Ingredients:</p>
-          <ul>{ingredients}</ul>
+          {/* <ul>{ingredients}</ul> */}
         </span>
         <span className="cardSpan step-list">
           <p>Steps:</p>
-          <ol>{steps}</ol>
+          {/* <ol>{steps}</ol> */}
         </span>
       </div>
     </div>
