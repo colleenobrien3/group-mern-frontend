ff770f7eee3abca1d89f27be00fdd01f27a9abe8
caio_ingber <caioingber@gmail.com>
2020-03-10 20:29:02 -0400

    fixed bug with blank inputs

--- src/reactComponents/Create/Create.js
+++ src/reactComponents/Create/Create.js
@@ -14,66 +14,109 @@ class Create extends Component {
       image: "",
       ingName: "",
       measurementValue: "",
-      measurementType: ""
+      measurementType: "",
+      emptyValues: false,
+      ingredeLength: 0
     };
   }
+  //Setting input value to state
   setInput = e => {
-    let place = e.target.placeholder;
-    e.preventDefault();
-    this.setState({ [place]: e.target.value });
+    let value = e.target.className;
+    e.persist();
+    // e.preventDefault();
+    this.setState({ [value]: e.target.value });
   };
-
+  // Creating ingredient object and clearing ingredient input
   setIngredient = e => {
     e.preventDefault();
-    let ingredient = {
-      name: this.state.ingName,
-      measurementValue: this.state.measurementValue,
-      measurementType: this.state.measurementType
-    };
-    let all = this.state.ingredients;
-    all.push(ingredient);
-    this.setState({ ingredients: all });
+    if (
+      !this.state.ingName ||
+      !this.state.measurementValue ||
+      !this.state.measurementType
+    ) {
+      console.log("no");
+    } else {
+      e.persist();
+      let ingredient = {
+        name: this.state.ingName,
+        measurementValue: this.state.measurementValue,
+        measurementType: this.state.measurementType
+      };
+      let all = this.state.ingredients;
+      all.push(ingredient);
+      this.setState({
+        ingredients: all,
+        ingredeLength: this.state.ingredeLength + 1,
+        ingName: "",
+        measurementValue: "",
+        measurementType: ""
+      });
+      console.log(e);
+      for (let i = 0; i < e.target.elements.length; i++) {
+        e.target.elements[i].value = "";
+      }
+    }
   };
 
   render() {
+    let list = this.state.ingredients.map(ingredient => {
+      if (this.state.ingredeLength === 0) {
+        return null;
+      } else {
+        return (
+          <li
+            key={ingredient.name}
+          >{`${ingredient.measurementValue} ${ingredient.measurementType} of ${ingredient.name}`}</li>
+        );
+      }
+    });
+
     console.log(this.state);
     return (
-      <form className="create-recipe" id="thisContainer">
-        <h2>New Recipe</h2>
-        <input
-          type="text"
-          placeholder="author"
-          onChange={this.setInput}
-        ></input>
-        <input
-          type="text"
-          placeholder="cuisine"
-          onChange={this.setInput}
-        ></input>
-        <input type="text" placeholder="name" onChange={this.setInput}></input>
-        <input
-          type="text"
-          placeholder="cooktime"
-          onChange={this.setInput}
-        ></input>
-        <input type="text" placeholder="image" onChange={this.setInput}></input>
-        <input
-          type="text"
-          placeholder="ingName"
-          onChange={this.setInput}
-        ></input>
-        <input
-          type="text"
-          placeholder="measurementValue"
-          onChange={this.setInput}
-        ></input>
-        <input
-          type="text"
-          placeholder="measurementType"
-          onChange={this.setInput}
-        ></input>
-        <button type="submit" onClick={this.setIngredient}></button>
-      </form>
+      <div>
+        <form className="create-recipe" id="thisContainer">
+          <h2>New Recipe</h2>
+          <input
+            type="text"
+            className="author"
+            onChange={this.setInput}
+          ></input>
+          <input
+            type="text"
+            className="cuisine"
+            onChange={this.setInput}
+          ></input>
+          <input type="text" className="name" onChange={this.setInput}></input>
+          <input
+            type="text"
+            className="cooktime"
+            onChange={this.setInput}
+          ></input>
+          <input type="text" className="image" onChange={this.setInput}></input>
+        </form>
+        <form onSubmit={this.setIngredient}>
+          <input
+            type="text"
+            placeholder="name (e.g. paprika, flour, chicken)"
+            onChange={this.setInput}
+            className="ingName"
+          ></input>
+          <input
+            type="number"
+            placeholder="value (e.g. 2, 2.25, 4)"
+            onChange={this.setInput}
+            className="measurementValue"
+          ></input>
+          <input
+            type="text"
+            placeholder="type (e.g. cups, tsp, tbsp)"
+            onChange={this.setInput}
+            className="measurementType"
+          ></input>
+          <button type="submit"> + </button>
+        </form>
+        <ul>{list}</ul>
+      </div>
     );
   }
 }
