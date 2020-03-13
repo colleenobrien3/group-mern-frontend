5485ebb5a5a0092afa4d0710395340e8f917ab28
RachelIsrael <racheleisrael4@gmail.com>
2020-03-13 09:34:02 -0400

    fixed "problems"

--- src/reactComponents/Home/Home.js
+++ src/reactComponents/Home/Home.js
@@ -1,8 +1,6 @@
 import React from "react";
 import "./Home.css";
 import Input from "../../storybookComponents/Input/Input";
-import RecipeCard from "../../storybookComponents/RecipeCard/RecipeCard";
-import CardContainer from "../CardContainer/CardContainer";
 
 const Home = props => {
   return (
