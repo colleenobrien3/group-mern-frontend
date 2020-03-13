815173690de65b6ab96a6a03aaf88ed6cfb85b12
Levani Papashvili <papashvililevani@gmail.com>
2020-03-12 21:18:39 -0400

    fixed responsive design

--- src/reactComponents/App/App.js
+++ src/reactComponents/App/App.js
@@ -115,7 +115,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> */}
           <Button label="Home" />
           
            <img src={logo} className="App-logo" alt="logo" />  
